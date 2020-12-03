e620ef80da4ea2772e16d6f57c83b902ed96a80a
someguy100 <75323664+someguy100@users.noreply.github.com>
2020-12-03 08:08:59 -0700

    Add files via upload
    My video game

---
+++ GameV1/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.ninjastar = New System.Windows.Forms.PictureBox()
+        Me.ninja = New System.Windows.Forms.PictureBox()
+        Me.Badguy = New System.Windows.Forms.PictureBox()
+        Me.score_label = New System.Windows.Forms.Label()
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.ninjastar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ninja, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Badguy, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        '
+        'ninjastar
+        '
+        Me.ninjastar.BackColor = System.Drawing.Color.Transparent
+        Me.ninjastar.Image = Global.GameV1.My.Resources.Resources.ninjastar1
+        Me.ninjastar.Location = New System.Drawing.Point(666, 222)
+        Me.ninjastar.Name = "ninjastar"
+        Me.ninjastar.Size = New System.Drawing.Size(66, 62)
+        Me.ninjastar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.ninjastar.TabIndex = 4
+        Me.ninjastar.TabStop = False
+        Me.ninjastar.Visible = False
+        '
+        'ninja
+        '
+        Me.ninja.BackColor = System.Drawing.Color.Transparent
+        Me.ninja.Image = Global.GameV1.My.Resources.Resources.ninjasword1
+        Me.ninja.Location = New System.Drawing.Point(633, 320)
+        Me.ninja.Name = "ninja"
+        Me.ninja.Size = New System.Drawing.Size(224, 179)
+        Me.ninja.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.ninja.TabIndex = 2
+        Me.ninja.TabStop = False
+        '
+        'Badguy
+        '
+        Me.Badguy.BackColor = System.Drawing.Color.Transparent
+        Me.Badguy.Image = Global.GameV1.My.Resources.Resources.bad_guy_rat3
+        Me.Badguy.Location = New System.Drawing.Point(178, 189)
+        Me.Badguy.Name = "Badguy"
+        Me.Badguy.Size = New System.Drawing.Size(116, 150)
+        Me.Badguy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Badguy.TabIndex = 1
+        Me.Badguy.TabStop = False
+        '
+        'score_label
+        '
+        Me.score_label.AutoSize = True
+        Me.score_label.Location = New System.Drawing.Point(35, 29)
+        Me.score_label.Name = "score_label"
+        Me.score_label.Size = New System.Drawing.Size(39, 13)
+        Me.score_label.TabIndex = 5
+        Me.score_label.Text = "Label1"
+        '
+        'Timer3
+        '
+        Me.Timer3.Enabled = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(783, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.GameV1.My.Resources.Resources.floating_islands___animated_version_1
+        Me.ClientSize = New System.Drawing.Size(902, 662)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.score_label)
+        Me.Controls.Add(Me.ninjastar)
+        Me.Controls.Add(Me.ninja)
+        Me.Controls.Add(Me.Badguy)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.ninjastar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ninja, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Badguy, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Badguy As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ninja As PictureBox
+    Friend WithEvents ninjastar As PictureBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents score_label As Label
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Label1 As Label
+End Class
---
+++ GameV1/Form1.vb
@@ -0,0 +1,176 @@
+﻿Public Class Form1
+    Dim r As New Random
+    Dim score As Integer
+    Dim time As Integer
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles Badguy.Click
+
+    End Sub
+    Sub randmove(p As PictureBox)
+        Dim x As Integer
+        Dim y As Integer
+        x = r.Next(-5, 10)
+        y = r.Next(-5, 10)
+        MoveTo(p, x, y)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        randmove(Badguy)
+    End Sub
+
+    Private Sub PictureBox2_Click_1(sender As Object, e As EventArgs) Handles ninja.Click
+        ninja.Location = New Point(ninja.Location.X + 3 = ninja.Location.Y)
+        If Collision(ninja, "wall") Then
+            MsgBox("out of bounds")
+        End If
+        If Collision(Badguy, "wall") Then
+            MsgBox("out of bounds")
+        End If
+    End Sub
+
+    Sub follow(p As PictureBox, x As Integer, y As Integer)
+
+    End Sub
+
+    Private Sub Form1_keydown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+
+        Select Case e.KeyCode
+            Case Keys.R
+                ninja.Image.RotateFlip(RotateFlipType.Rotate90FlipX)
+                Me.Refresh()
+
+            Case Keys.Up
+                MoveTo(ninja, 0, -5)
+
+            Case Keys.Down
+                MoveTo(ninja, 0, 5)
+
+            Case Keys.Left
+                MoveTo(ninja, 5, 0)
+
+            Case Keys.Right
+                MoveTo(ninja, -5, 0)
+
+            Case Keys.Space
+                ninjastar.Location = ninja.Location
+                ninjastar.Visible = True
+
+        End Select
+
+    End Sub
+
+    Private Sub PictureBox2_Click_2(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Public Sub X()
+
+        Dim bdir As Integer = 6
+        If ninjastar.Location.X < 0 Then
+            bdir = 5
+
+        End If
+        If ninjastar.Location.X > 3000 Then
+            bdir = -5
+        End If
+    End Sub
+    Public Sub follow(p As PictureBox)
+        Dim x, y As Integer
+        If p.Location.X > Badguy.Location.X Then
+            x = -5
+        Else
+            x = 5
+        End If
+        MoveTo(p, x, 0)
+        If p.Location.Y < Badguy.Location.Y Then
+            y = 5
+        Else
+            y = -5
+        End If
+        MoveTo(p, x, y)
+    End Sub
+
+
+
+    Function Collision(p As PictureBox, t As String, Optional ByRef other As Object = vbNull)
+        Dim col As Boolean
+
+        For Each c In Controls
+            Dim obj As Control
+            obj = c
+            If obj.Visible AndAlso p.Bounds.IntersectsWith(obj.Bounds) And obj.Name.ToUpper.Contains(t.ToUpper) Then
+                col = True
+                other = obj
+            End If
+        Next
+        Return col
+    End Function
+
+    'Return true or false if moving to the new location is clear of objects ending with t
+    Function IsClear(p As PictureBox, distx As Integer, disty As Integer, t As String) As Boolean
+        Dim b As Boolean
+
+        p.Location += New Point(distx, disty)
+        b = Not Collision(p, t)
+        p.Location -= New Point(distx, disty)
+        Return b
+    End Function
+    'Moves and object (won't move onto objects containing  "wall" and shows green if object ends with "win"
+    Sub MoveTo(p As PictureBox, distx As Integer, disty As Integer)
+
+
+        If IsClear(p, distx, disty, "WALL") Then
+            p.Location += New Point(distx, disty)
+        End If
+
+        If p.Name = "ninjastar" And Collision(p, "Badguy") Then
+            score += 1
+
+        End If
+
+
+    End Sub
+
+    Private Sub ninjastar_Click(sender As Object, e As EventArgs) Handles ninjastar.Click
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        MoveTo(ninjastar, -20, 0)
+        ninjastar.Image.RotateFlip(RotateFlipType.Rotate90FlipX)
+
+
+        score_label.Text = score
+        If score = 50 Then
+            MsgBox("YOU WIN!")
+            Me.BackColor = Color.Green
+            Me.Visible = True
+        End If
+
+
+    End Sub
+
+    Sub CreateNew(name As String, pic As PictureBox, location As Point)
+        Dim p As New PictureBox
+        p.Location = location
+        p.Image = pic.Image
+        p.BackColor = pic.BackColor
+        p.Name = name
+        p.Width = pic.Width
+        p.Height = pic.Height
+        p.SizeMode = PictureBoxSizeMode.StretchImage
+        Controls.Add(p)
+
+    End Sub
+
+
+End Class
+
+
+
+
